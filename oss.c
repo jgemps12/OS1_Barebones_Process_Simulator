75c4b602812f7466f1d6814d79b0489fe6f65f84
jesse gempel <gempel@opsys.cs.umsl.edu>
2025-02-06 15:59:22 -0600

    oss.c: added preprocessor directives and main() function

---
+++ oss.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv) {
+
+   return EXIT_SUCCESS;
+
+}
+
+
