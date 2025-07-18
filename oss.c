2381bd3f69065e2823da367bf45e4cf47285c80e
jgemps12 <jessegempel@gmail.com>
2025-07-17 22:57:58 -0500

    Restore oss.c from version from commit 4f0f887

--- oss.c
+++ oss.c
@@ -9,8 +9,6 @@
 #include <string.h>
 #include <stdbool.h>
 
-
-
 int main(int argc, char** argv) {
    
    int opt; 
@@ -20,44 +18,37 @@ int main(int argc, char** argv) {
    int simul = 1;
    int iter = 1;
 
-
    // If user puts invalid values for opt arguments, one of these strings will be passed into 'checkForOptargError()'.
    // This will help state the option in which the error occurred while trying to execute './oss'.
    char procName[] = "-n [proc]";
    char simulName[] = "-s [simul]";
    char iterName[] = "-t [iter]";
 
-
    while ((opt = getopt(argc, argv, "hn:s:t:")) != -1) {
       switch (opt) {
          case 'h':
             printHelpMessage();
-
 	    break;
 
          case 'n':
             proc = atoi(optarg);
 	    checkForOptargEntryError(proc, procName);
-
             break;
 
          case 's':
             simul = atoi(optarg);
             checkForOptargEntryError(simul, simulName);
 	    checkForSimulExceedsProcError(simul, proc);
-
             break;
 
          case 't':
             iter = atoi(optarg);
             checkForOptargEntryError(iter, iterName);
-
             break;
 
          default:
 	    printf("ERROR in oss.c: Arguments are invalid or you forgot to input a value for them.\n");
 	    printf("Please type './oss -h' for help.\n\n");
-
             exit(-1);
 
  	    break;
@@ -68,23 +59,17 @@ int main(int argc, char** argv) {
    int childrenActive = 0;                                          // # of children running simultaneously (not to be confused with 'proc').
    int totalChildrenLaunched = 0;                                   // # of children launched so far (not to be confused with 'simul').
 
-
    // Stores the total iteration count into string to facilatate execl()'s operation.
    char iteration_count[50];                                     
    sprintf(iteration_count, "%d", iter);
 
-
    while (processesFinished == false) {
-
-
       // If not all possible children are run yet AND the # of simultaneous children is less than maximum, continue running processes.
       if (childrenActive < simul && totalChildrenLaunched < proc) {
-
-
+	      
          // Split a child process from a parent.
          pid_t childPid = fork();
       
-	 
 	 // Work with child process. Send [iter] to user.c to execute the child process.
 	 if (childPid == 0) {
             execl("./user", "user", iteration_count, NULL);
@@ -92,21 +77,18 @@ int main(int argc, char** argv) {
 	    printf("ERROR in oss.c: the execl() function has failed. Terminating program.\n\n");
 	    exit(-1);
 	 }
-
-
+		 
 	 // Work with parent process. Increment the current # of total children and those running simultaneously.
 	 else if (childPid > 0) {
             childrenActive++;
 	    totalChildrenLaunched++;
 	 }
 
-
 	 else {
             printf("ERROR in oss.c: the fork() function has failed. Terminating program.\n\n");
 	    exit(-1);
 	 }
       }  
-
 
       // If no more children are running and the maximum # of total children have been launched, end loop/program.
       if (childrenActive == 0 && totalChildrenLaunched == proc) {
@@ -115,22 +97,18 @@ int main(int argc, char** argv) {
          processesFinished = true;
       }
  
-
       // If the limit of simultaneous children has been reached, but more still need to be launched, wait for them to terminate.
       if (childrenActive == simul && totalChildrenLaunched < proc) {
-         wait(0);
+         waitpid(-1, NULL, 0);
 	 childrenActive--;
       }
 
-
       // If all available children have launched, but not all of them finished, wait for them to terminate.
       if (childrenActive > 0 && totalChildrenLaunched == proc) {
-	 wait(0);
+	 waitpid(-1, NULL, 0);
 	 childrenActive--; 
       }
    }
 
    return EXIT_SUCCESS;
 }
-
-
