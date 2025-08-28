b9d8ac29d89d0327c3fceeb69465d9964eff207f
jgemps12 <jessegempel@gmail.com>
2025-08-27 20:16:10 -0500

    Fix indenting issues
    Code needed to be fixed as shown on GitHub by removing spaces and replacing them all with tabs.

--- functions.c
+++ functions.c
@@ -9,12 +9,12 @@
 void checkForOptargEntryError(int value, char getoptArgument[]) {
    if ((value <= 0 || value > 10)  && (strcmp(getoptArgument, "-n [proc]") == 0)) {
       printf("ERROR in oss.c: User must enter an integer between 1 and 10 for argument %s.\n\n", getoptArgument);
-
+      
       exit(-1);
    }
    if (value <= 0 && (strcmp(getoptArgument, "-n [proc]") != 0)) {
       printf("ERROR in oss.c: User must enter a positive integer for argument %s.\n\n", getoptArgument);
-
+      
       exit(-1);
    }
 }
@@ -23,7 +23,7 @@ void checkForOptargEntryError(int value, char getoptArgument[]) {
 void checkForSimulExceedsProcError(int simulProcesses, int totalProcesses) {
    if (simulProcesses > totalProcesses) {
       printf("ERROR in oss.c: The -s [simul] value '%d' cannot be greater than the -n [proc] value '%d'.\n\n", simulProcesses, totalProcesses);
-
+      
       exit(-1);
    }
 }
@@ -35,18 +35,18 @@ void printHelpMessage() {
    printf("\t1.) Process IDs (or PIDs).\n");
    printf("\t2.) Parent process IDs (or PPIDs).\n");
    printf("\t3.) The iteration of a loop that displays PIDs and PPIDs.\n\n");
-
+   
    printf("\n\nTo execute this program, type './oss', then type in any combination of options:\n");
    printf("Option:           What to enter after option:              Required (yes/no):             Description:\n");
    printf("  -h                nothing.                                 no                             Displays this help menu.\n");
    printf("  -n [proc]         an integer between 1 and 10.             no; defaults to 1.             Runs a total # of processes.\n");
    printf("  -s [simul]        an integer smaller than '-n [proc]'.     no; defaults to 1.             Runs a max # of processes simultaneously.\n");
    printf("  -t [iter]         an integer of at least 1.                no; defaults to 1.             Iterates through all processes a total # of times.\n\n");
-
+   
    printf("For example, typing './oss -n 6 -s 4 -t 2' will run:\n");
    printf("\t1.) a total of 6 processes.\n");
    printf("\t2.) a maximum of 4 processes simultaneously.\n");
    printf("\t3.) a total of 2 iterations (i.e., 6 processes will begin and terminate twice, with up to 4 running simultaneously).\n\n\n");
-
+   
    exit(0);
 }
